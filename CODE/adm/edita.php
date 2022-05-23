113715a09919861165d4f7e68ebd6474c21465cf
carlostomazin <carlos.tomazin2@gmail.com>
2022-05-23 03:10:29 -0300

    atualizacoes

--- CODE/adm/edita.php
+++ CODE/adm/edita.php
@@ -44,21 +44,79 @@ foreach ($linha as $func) {  //a cada linha, ele percorre o banco de dados (a pa
             </div>
             <br>
             <div class="form-group">
-                <select class="form-select" name="status" aria-label="Default select example">
+                <select class="form-select" name="status" aria-label="Default select example" onchange="muda(this);">
                     <option selected> Status atual: <?php echo $status ?></option>
-                    <option value="Aguardando">Aguardando</option>
+                    <option value="analise">Em análise</option>
                     <option value="progresso">Em progresso</option>
-                    <option value="Finalizado">Finalizado</option>
+                    <option value="finalizado">Finalizado</option>
                 </select>
+            </div>
+            <div class="form-group" id="caixa1" style="display: none;">
+                <br> <label> Informações de fechamento: </label> <br>
+                <div class="form-floating">
+                    <textarea name="inf_fechamento" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px" require></textarea>
+                </div>
             </div>
             <br>
             <div style="text-align: right">
                 <a class="btn btn-sm btn-primary" href="../menuadm.php">voltar</a>
-                <button type="submit" class="btn btn-sm btn-success"> Atualizar </button>
+                <button type="submit" class="btn btn-sm btn-success" id="button_att"> Atualizar </button>
             </div>
+        
+            <!-- Modal -->
+            <div class="modal fade" id="modalExemplo<?php echo$id?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente finalizar esse chamado?</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Ao finalizar esse chamado não será mais possivel editar!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                <button type="submit" class="btn btn-danger" id="button_att"> Finalizar </button>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            
         </form>
     </div>
 
-<?php
-}
-?>
+<?php } ?>
+
+<script type='text/javascript'>
+
+    function muda(obj){ 
+        var index = obj.selectedIndex; 
+        var option = obj.options[index].value; 
+        if (option=='finalizado') { 
+    			document.getElementById('caixa1').style.display="block";
+                document.getElementById('button_att').setAttribute('type', 'button');
+                document.getElementById('button_att').setAttribute("data-toggle", "modal");
+                document.getElementById('button_att').setAttribute("data-target", "#modalExemplo<?php echo$id?>");
+
+        }else
+        if (option=='analise') { 
+    			document.getElementById('caixa1').style.display="none"; 
+                document.getElementById('button_att').setAttribute('type', 'submit');
+                document.getElementById('button_att').removeAttribute("data-toggle");
+                document.getElementById('button_att').removeAttribute("data-target");
+    	} else
+        if (option=='progresso') { 
+    			document.getElementById('caixa1').style.display="none";
+                document.getElementById('button_att').setAttribute('type', 'submit');
+                document.getElementById('button_att').removeAttribute("data-toggle");
+                document.getElementById('button_att').removeAttribute("data-target");
+    	}
+    }
+
+</script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- CODE/menuadm.php
+++ CODE/menuadm.php
@@ -53,55 +53,34 @@
             ?>
 
                 <tr style="text-align: center;">
-                    <td> <?php echo $id ?> </td>
-                    <td> <?php echo $detalhes ?> </td>
-                    <td> <?php echo $data ?> </td>
-                    <td> <?php echo $status ?> </td>
+                    <td> <?php echo $id         ?>  </td>
+                    <td> <?php echo $detalhes   ?>  </td>
+                    <td> <?php echo $data       ?>  </td>
+                    <td> <?php echo $status     ?>  </td>
                     <td>
-
-
-                        <a class="btn btn-success btn-sm" style="color:white" href="adm/edita.php?id=<?php echo $id ?>" role="button"><i class="fa-solid fa-pen-to-square"></i> Editar </a>
-                        <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#modalExemplo<?php echo$id?>"> Deletar </button>
+                        <a class="btn btn-primary btn-sm" href="user/chamados.php?id=<?php echo $id?>" role="button"> visualizar </a>
+                        <a class="btn btn-success btn-sm" href="adm/edita.php?id=<?php echo $id ?>" role="button" id="button_edit"> Editar </a>
+                        <script> 
+                            if ("<?php echo $status ?>" == 'finalizadoa'){
+                                document.getElementById('button_edit').setAttribute('style', 'display: none;');
+                            }
+                        </script>
                     </td>
                 </tr>
-
-                <!-- Modal -->
-                <div class="modal fade" id="modalExemplo<?php echo$id?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Deseja realmente deletar esse chamado?</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                Ao deletar esse chamado todas as informações serão perdidas
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                                <a class="btn btn-danger"  href="adm/deleta.php?id=<?php echo $id?>" role="button"><i class="fa-solid fa-trash"></i> Excluir </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php
-            }
-            ?>
+                
+            <?php } ?>
 
         </table>
 
-        <br>
-        <a class="btn btn-danger" href="scripts/sair.php">Sair</a>
+        <br> <a class="btn btn-danger" href="scripts/sair.php">Sair</a>
+
     </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
-
-
-
-
 
 
 </html>
